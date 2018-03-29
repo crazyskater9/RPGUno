2ba439c30c204ecb2d4130481274ea2fd5854efb
Markus Bointner <bointner.markus9@gmail.com>
2018-03-29 18:22:05 +0200

    Erster Test mit JPanel

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+
+class MyPanel extends JPanel {
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.setFont(new Font("Dialog", Font.PLAIN, 18));
+        g.setColor(Color.BLUE);
+        g.drawString("Hallo Welt", 10,40);
+    }
+}
+
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+
+        JFrame f = new JFrame("Titel");
+        f.setVisible(true);
+        f.setSize(600, 800);
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JPanel p = new MyPanel();
+
+        f.add(p);
+
+
     }
 }
