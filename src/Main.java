6ab95b87224365342a274d77f2ed6156478d0c7a
Markus Bointner <bointner.markus9@gmail.com>
2018-03-29 16:11:11 +0200

    JFrame Testerei

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        JFrame f = new JFrame("Test");
+        f.setVisible(true);
+        f.setSize(600, 800);
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JPanel p = new JPanel();
+
+
+
+
+
+
+
+
+        f.add(p);
+
+
+
     }
 }
