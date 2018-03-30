a9bca2cc7328d530629987655e90ed676a54545b
Gratla <philippleeb@gmail.com>
2018-03-30 21:09:07 +0200

    Update Projectile.java -Added Constructor -Added move() -Added getPlayerPosition()
    Added SmallBullet.java
    -Added Constructor
    -Added debugValues()

--- src/Game/Projectile.java
+++ src/Game/Projectile.java
@@ -1,4 +1,27 @@
 package Game;
 
 public class Projectile {
+
+    public int lifeTime;
+    public int damageOnHit;
+    public Vector2D position;
+    public Vector2D movement;
+
+    public Projectile(int lifeTime, int damageOnHit, Vector2D position, Vector2D movement)
+    {
+        this.lifeTime = lifeTime;
+        this.damageOnHit = damageOnHit;
+        this.position = position;
+        this.movement = movement;
+    }
+
+    public void move()
+    {
+        position.add(movement);
+    }
+
+    public Vector2D getPlayerPosition(Vector2D playerPosition)
+    {
+        return playerPosition;
+    }
 }
